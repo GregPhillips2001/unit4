4732723fcb3891d049be990b773b3ee4b0d61828
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-10-16 14:32:31 -0400

    Updated from Brython Server: 10/16/2017 2:32:30 PM

--- rectangle.py
+++ rectangle.py
@@ -1,1 +1,10 @@
+#Greg Phillips
+#10/16/17
+#rectangle.py
 
+def rectangle(width,height):
+    area = width*height
+    perimeter = (2*width)+(2*height)
+    print("The area is", area)
+    print("The perimeter is", perimeter)
+rectangle(3,4)
