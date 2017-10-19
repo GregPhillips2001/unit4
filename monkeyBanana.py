52fdf8ff59c5a6e79192cd4a6696c93e62b02f23
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-10-19 14:10:55 -0400

    Updated from Brython Server: 10/19/2017 2:10:53 PM

--- monkeyBanana.py
+++ monkeyBanana.py
@@ -4,8 +4,12 @@
 
 from ggame import *
 
-if __name__ == "__maine__":
+if __name__ == "__main__":
     
     green = Color(0x006600,1)
     
     jungleBox = RectangleAsset(800,600,LineStyle(1,green),green)
+    
+    Sprite(jungleBox)
+    
+    App().run()
