53166226a22b3b085c72d40a0c8a3b03da842696
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-10-23 14:18:54 -0400

    Updated from Brython Server: 10/23/2017 2:18:53 PM

--- localDemo.py
+++ localDemo.py
@@ -1,1 +1,14 @@
+#Greg Phillips
+#10/23/17
+#localDemo.py - understanding local variables
+
+def f():
+    x = 77 #x is a local variable
+    y = 10 #y is a local variable --- Variable that gets declared inside of the function 
+    
+x = 5
+f() #x does not change 
+print(x)
+print(y) #this will cause an error
+
 
