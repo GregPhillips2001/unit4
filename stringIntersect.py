b4947ccf57c813d2c80d63cadee6e0f2dac91fd8
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-10-25 13:45:07 -0400

    Updated from Brython Server: 10/25/2017 1:45:06 PM

--- stringIntersect.py
+++ stringIntersect.py
@@ -5,13 +5,8 @@
 def stringIntersect(word1, word2):
     total = " "
     for ch in word1:
-        if ch in total:
+        if ch in word2:
             total += ch
-            
-    for ch in word2:
-        if ch in total:
-            total += ch
-    
     return total
 
 total = stringIntersect("greg", "game")
