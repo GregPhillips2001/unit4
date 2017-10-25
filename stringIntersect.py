61614ba3e671e52c4415e7b20ca15b73f5448c31
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-10-25 13:43:46 -0400

    Updated from Brython Server: 10/25/2017 1:43:45 PM

--- stringIntersect.py
+++ stringIntersect.py
@@ -1,1 +1,18 @@
+#Greg Phillips
+#10/23/17
+#stringUnion.py
 
+def stringIntersect(word1, word2):
+    total = " "
+    for ch in word1:
+        if ch in total:
+            total += ch
+            
+    for ch in word2:
+        if ch in total:
+            total += ch
+    
+    return total
+
+total = stringUnion("greg", "game")
+print(total)
