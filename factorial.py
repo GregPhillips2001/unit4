76d7aa84cf55f9245248c12ad111692d7dd820e7
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-10-26 14:45:48 -0400

    Updated from Brython Server: 10/26/2017 2:45:47 PM

--- factorial.py
+++ factorial.py
@@ -12,3 +12,10 @@ def factorial(n):
     return total
 print(factorial(5))
         
+def factorial(n):
+    total = n
+    while n>=2:
+        n-=1
+        total = total*n
+    return total
+print(factorial(5))
