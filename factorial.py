a02eac0a0f80975bec77b7a9fa33386c2e929829
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-10-26 14:25:58 -0400

    Updated from Brython Server: 10/26/2017 2:25:58 PM

--- factorial.py
+++ factorial.py
@@ -1,1 +1,13 @@
+#greg phillips
+#10/26/17
+#factorial.py
 
+def factorial(n):
+    total = n
+    if n == 0:
+        return total
+    else:
+        total = total*(n-1)
+        factorial(n-1)
+factorial(5)
+    
