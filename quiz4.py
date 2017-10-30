c96faa0cd0694bfceb98e92b85d32f5729227462
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-10-30 14:32:00 -0400

    Updated from Brython Server: 10/30/2017 2:31:59 PM

--- quiz4.py
+++ quiz4.py
@@ -5,3 +5,13 @@
 def csia():
     print("Computer Science is Awesome " *5)
 csia()
+
+def average(x,y,z):
+    return (x+y+z)/3
+print(average(1,2,3))
+
+def lastLetter(word):
+    for ch in word:
+        print(ch)
+lastLetter("Greg")
+    
